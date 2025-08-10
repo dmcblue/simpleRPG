d354057383608abe3610050644cd133a4771c787
dmcblue <dmcblue@gmail.com>
2025-08-10 11:50:38 -0400

    Builder Code Clean Up 2

---
+++ builder/src/conversations.rs
@@ -0,0 +1,119 @@
+// int
+use std::fs::File;
+use std::io::Write;
+use std::clone::Clone;
+
+// ext
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
+pub struct ConversationNode {
+	id: usize,
+	prompt: String,
+	response: String,
+	after: Option<String>,
+	enabled: bool,
+	prompts: Vec<ConversationNode>,
+}
+
+pub struct ConversationsFile {
+	file_handle: File
+}
+
+impl ConversationsFile {
+	pub fn new() -> Self {
+		Self {
+			file_handle: File::create("../game/src/data/conversations.rs").unwrap(),
+		}
+	}
+
+	pub fn begin(&mut self) {
+		let _ = self.file_handle.write_all(
+			b"use super::components::Components;\n\n\
+			pub struct ConversationRoot {\n\
+			\tid: usize,\n\
+			\tprompts: Vec<ConversationNode>,
+			}\n\n\
+			pub struct ConversationNode {\n\
+			\tid: usize,\n\
+			\tprompt: String,\n\
+			\tresponse: String,\n\
+			\tprompts: Vec<ConversationNode>,\
+			}\n\n\
+			impl ConversationRoot {\n\
+			\tpub fn new () -> Self {\n\
+			\t\treturn ConversationRoot {\n\
+			\t\t\tid: 0,\n\
+			\t\t\tprompts: Vec::new(),\n\
+			\t\t};\n\
+			\t}\n\
+			}\n\n\
+			pub fn load_conversations(components: &mut Components) {\n\
+			\tcomponents.conversations = [\n"
+		);
+	}
+
+	pub fn end(&mut self) {
+		let _ = self.file_handle.write_all(
+			b"\t];\n}\n"
+		);
+	}
+
+	pub fn open_root(&mut self, uuid: usize) {
+		let _ = self.file_handle.write_all(
+			format!(
+				"\t\tConversationRoot{{\n\
+				\t\t\tid: {},\n\
+				\t\t\tprompts: vec![\n\
+				",
+				uuid
+			).as_bytes()
+		);
+	}
+
+	pub fn close_root(&mut self) {
+		let _ = self.file_handle.write_all(
+			b"\t\t\t]\n\t\t},\n"
+		);
+	}
+
+
+	// needs to be iterative to not be awful
+	pub fn render_conversation(
+		&mut self,
+		conversation: &ConversationNode,
+		depth: String
+	) {
+		let _ = self.file_handle.write_all(
+			format!(
+				"{}\t\t\t\tConversationNode{{\n\
+				{}\t\t\t\t\tid: {},\n\
+				{}\t\t\t\t\tprompt: String::from(\"{}\"),\n\
+				{}\t\t\t\t\tresponse: String::from(\"{}\"),\n\
+				{}\t\t\t\t\tprompts: vec![\n",
+				depth,
+				depth,
+				conversation.id,
+				depth,
+				conversation.prompt,
+				depth,
+				conversation.response,
+				depth,
+			).as_bytes()
+		);
+		for c in conversation.prompts.clone() {
+			self.render_conversation(
+				&c,
+				format!("\t\t{}", depth)
+			);
+		}
+		let _ = self.file_handle.write_all(
+			format!(
+				"{}\t\t\t\t\t]\n\
+				{}\t\t\t\t}}\n",
+				depth,
+				depth
+			).as_bytes()
+		);
+	}
+}
--- builder/src/main.rs
+++ builder/src/main.rs
@@ -13,6 +13,9 @@ use serde_yml;
 mod components;
 use components::{write_components_file};
 
+mod conversations;
+use conversations::{ConversationNode, ConversationsFile};
+
 mod counts;
 use counts::Counts;
 
@@ -23,15 +26,7 @@ const ENTITY_TYPE_ITEM: &str = "Item";
 const ENTITY_TYPE_LOCATION: &str = "Location";
 const ENTITY_TYPE_PERSON: &str = "Person";
 
-#[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
-struct ConversationNode {
-	id: usize,
-	prompt: String,
-	response: String,
-	after: Option<String>,
-	enabled: bool,
-	prompts: Vec<ConversationNode>,
-}
+
 
 #[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
 struct Entity {
@@ -60,30 +55,8 @@ fn main() {
 		b"use super::components::Components;\n\n\
 		pub fn load_data(components: &mut Components) {\n"
 	);
-	let mut conversations_file = File::create("../game/src/data/conversations.rs").unwrap();
-	let _ = conversations_file.write_all(
-		b"use super::components::Components;\n\n\
-		pub struct ConversationRoot {\n\
-		\tid: usize,\n\
-		\tprompts: Vec<ConversationNode>,
-		}\n\n\
-		pub struct ConversationNode {\n\
-		\tid: usize,\n\
-		\tprompt: String,\n\
-		\tresponse: String,\n\
-		\tprompts: Vec<ConversationNode>,\
-		}\n\n\
-		impl ConversationRoot {\n\
-		\tpub fn new () -> Self {\n\
-		\t\treturn ConversationRoot {\n\
-		\t\t\tid: 0,\n\
-		\t\t\tprompts: Vec::new(),\n\
-		\t\t};\n\
-		\t}\n\
-		}\n\n\
-		pub fn load_conversations(components: &mut Components) {\n\
-		\tcomponents.conversations = [\n"
-	);
+	let mut conversations_file = ConversationsFile::new();
+	conversations_file.begin();
 	let mut counts: Counts = Counts::new();
 	let mut entities: HashMap<usize, Entity> = HashMap::new();
 
@@ -95,58 +68,57 @@ fn main() {
 		if entity.entity_type == "Game" {
 			counts.starting_location_id = entity.location.unwrap();
 		} else {
-			let id: usize = entity.id.unwrap();
+			let uuid: usize = entity.id.unwrap();
 			match entity.entity_type.as_str() {
-				ENTITY_TYPE_CONVERSATION => { counts.conversations.push(id); },
-				ENTITY_TYPE_EXIT => { counts.exits.push(id); },
-				ENTITY_TYPE_ITEM => { counts.items.push(id); },
-				ENTITY_TYPE_LOCATION => { counts.locations.push(id); },
-				ENTITY_TYPE_PERSON => { counts.people.push(id); },
+				ENTITY_TYPE_CONVERSATION => { counts.conversations.push(uuid); },
+				ENTITY_TYPE_EXIT => { counts.exits.push(uuid); },
+				ENTITY_TYPE_ITEM => { counts.items.push(uuid); },
+				ENTITY_TYPE_LOCATION => { counts.locations.push(uuid); },
+				ENTITY_TYPE_PERSON => { counts.people.push(uuid); },
 				_ => ()
 			}
 			if entity.metaname == "Inventory" {
-				counts.inventory_uuid = id;
+				counts.inventory_uuid = uuid;
 			}
-			entities.insert(id, entity);
+			entities.insert(uuid, entity);
 		}
 	}
 
-	// uuid to array id
-	let mut id_map: HashMap<usize, usize> = HashMap::new();
+	let mut uuid_to_index: HashMap<usize, usize> = HashMap::new();
 
 	for entity_id in counts.locations.iter() {
-		id_map.insert(*entity_id, counts.total);
+		uuid_to_index.insert(*entity_id, counts.total);
 		counts.total = counts.total + 1;
 	}
 	counts.items_start = counts.total;
 	for entity_id in counts.items.iter() {
-		id_map.insert(*entity_id, counts.total);
+		uuid_to_index.insert(*entity_id, counts.total);
 		counts.total = counts.total + 1;
 	}
 	counts.people_start = counts.total;
 	for entity_id in counts.people.iter() {
-		id_map.insert(*entity_id, counts.total);
+		uuid_to_index.insert(*entity_id, counts.total);
 		counts.total = counts.total + 1;
 	}
 	counts.exits_start = counts.total;
 	for entity_id in counts.exits.iter() {
-		id_map.insert(*entity_id, counts.total);
+		uuid_to_index.insert(*entity_id, counts.total);
 		counts.total = counts.total + 1;
 	}
 	counts.conversations_start = counts.total;
 	for entity_id in counts.conversations.iter() {
-		id_map.insert(*entity_id, counts.total);
+		uuid_to_index.insert(*entity_id, counts.total);
 		counts.total = counts.total + 1;
 	}
-	for (id, entity) in entities {
-		let array_index = *id_map.get(&id).unwrap();
+	for (uuid, entity) in entities {
+		let array_index = *uuid_to_index.get(&uuid).unwrap();
 		// all non-conversations
 		if array_index < counts.conversations_start {
 			let _ = file.write_all(
 				format!(
 					"\tcomponents.uuids[{}] = {};\n",
 					array_index,
-					id
+					uuid
 				).as_bytes()
 			);
 			let _ = file.write_all(
@@ -169,7 +141,7 @@ fn main() {
 				let _ = file.write_all(
 					format!(
 						"\tcomponents.locations[{}].push({});\n",
-						id_map.get(&entity.location.unwrap()).unwrap(),
+						uuid_to_index.get(&entity.location.unwrap()).unwrap(),
 						array_index
 					).as_bytes()
 				);
@@ -177,7 +149,7 @@ fn main() {
 					format!(
 						"\tcomponents.location_map[{}] = {};\n",
 						array_index,
-						id_map.get(&entity.location.unwrap()).unwrap()
+						uuid_to_index.get(&entity.location.unwrap()).unwrap()
 					).as_bytes()
 				);
 			}
@@ -188,31 +160,19 @@ fn main() {
 			let _ = file.write_all(
 				format!(
 					"\tcomponents.owns_conversation[{}] = {};\n",
-					id_map.get(&entity.speaker.unwrap()).unwrap(),
+					uuid_to_index.get(&entity.speaker.unwrap()).unwrap(),
 					array_index,
 				).as_bytes()
 			);
-			let _ = conversations_file.write_all(
-				format!(
-					"\t\tConversationRoot{{\n\
-					\t\t\tid: {},\n\
-					\t\t\tprompts: vec![\n\
-					",
-					id
-				).as_bytes()
-			);
-			// let mut stack: Vec<ConversationNode> = Vec::new();
+
+			conversations_file.open_root(uuid);
 			for conversation in entity.prompts.unwrap() {
-				// needs to be iterative to not be awful
-				render_conversation(
-					&mut conversations_file,
+				conversations_file.render_conversation(
 					&conversation,
-					String::from("\t")
+					String::new()
 				);
 			}
-			let _ = conversations_file.write_all(
-				b"\t\t\t]\n\t\t},\n"
-			);
+			conversations_file.close_root();
 		}
 		// exits only
 		else if array_index >= counts.exits_start {
@@ -220,7 +180,7 @@ fn main() {
 				format!(
 					"\tcomponents.destinations[{}] = {};\n",
 					array_index - counts.exits_start,
-					id_map.get(&entity.to.unwrap()).unwrap()
+					uuid_to_index.get(&entity.to.unwrap()).unwrap()
 				).as_bytes()
 			);
 		}
@@ -243,54 +203,13 @@ fn main() {
 			"\n".as_bytes()
 		);
 	}
-	let _ = conversations_file.write_all(
-		b"\t];\n}\n"
-	);
+	conversations_file.end();
 
 	let _ = file.write_all(
 		format!(
 			"}}\n\npub fn get_start_location_id() -> usize {{ {} }}",
-			id_map.get(&counts.starting_location_id).unwrap()
+			uuid_to_index.get(&counts.starting_location_id).unwrap()
 		).as_bytes()
 	);
-	write_components_file(&counts, *id_map.get(&counts.inventory_uuid).unwrap());
-}
-
-fn render_conversation(
-	conversations_file: &mut File,
-	conversation: &ConversationNode,
-	depth: String
-) {
-	let _ = conversations_file.write_all(
-		format!(
-			"{}\t\tConversationNode{{\n\
-			{}\t\t\tid: {},\n\
-			{}\t\t\tprompt: String::from(\"{}\"),\n\
-			{}\t\t\tresponse: String::from(\"{}\"),\n\
-			{}\t\t\tprompts: vec![\n",
-			depth,
-			depth,
-			conversation.id,
-			depth,
-			conversation.prompt,
-			depth,
-			conversation.response,
-			depth,
-		).as_bytes()
-	);
-	for c in conversation.prompts.clone() {
-		render_conversation(
-			conversations_file,
-			&c,
-			format!("\t{}", depth)
-		);
-	}
-	let _ = conversations_file.write_all(
-		format!(
-			"{}\t\t\t]\n\
-			{}\t\t}}\n",
-			depth,
-			depth
-		).as_bytes()
-	);
+	write_components_file(&counts, *uuid_to_index.get(&counts.inventory_uuid).unwrap());
 }
